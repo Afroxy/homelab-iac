26ad4bd02bd3b725eae885663b784e7bd973469d
Benjamin Börschlein <87987738+Afroxy@users.noreply.github.com>
2025-03-08 04:32:13 +0100

    [ADD]: Working setup for backup NAS (banas1) running on bare-metal Debian

---
+++ ansible/templates/rsync_pull_job.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+## Run rsync job, retry on errors until a maximum number of retries is reached
+
+MAX_RETRIES=50
+RSYNC_PULL_COMMAND=(rsync -av {{ rsync_user }}@{{ rsync_host }}:{{ rsync_source_path }} {{ rsync_destination_path }} -A -X --inplace --delete --log-file="/var/log/rsync/rsync_pull_job_$(date +\%Y-\%m-\%d_\%H-\%M-\%S).log" )
+i=0
+
+while [ $i -lt $MAX_RETRIES ]; do
+  i=$((i + 1))
+  
+  # Run rsync command
+  "${RSYNC_PULL_COMMAND[@]}"
+  
+  # If rsync succeeds, break the loop
+  if [ $? -eq 0 ]; then
+    break
+  fi
+
+  echo "Rsync failed, retrying... ($i/$MAX_RETRIES)"
+  sleep 5  # Optional: Avoid hammering the server too quickly
+done
+
+# Exit with error if we hit max retries
+if [ $i -eq $MAX_RETRIES ]; then
+  echo "Hit maximum number of retries, giving up."
+  exit 1
+fi
+
+exit 0  # Success
+
+
