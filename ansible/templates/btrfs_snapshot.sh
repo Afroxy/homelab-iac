f07ae427317c693b1f1a3bd22a354f51fd17cc48
Benjamin Börschlein <87987738+Afroxy@users.noreply.github.com>
2025-03-23 01:39:47 +0100

    [UPDATE]: remove ansible variables from templates

---
+++ ansible/templates/btrfs_snapshot.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+
+# Check if a subvolume path is provided
+if [[ "$#" -ne 2 ]]; then
+    echo "Usage: $(basename "$0") <subvolume_path> <snapshot_parent>"
+    exit 1
+fi
+
+
+# Get absolute path of the subvolume
+SUBVOLUME_PATH=$(realpath "$1")
+SNAPSHOT_PARENT=$(realpath "$2")
+
+SUBVOLUME_NAME=$(basename "$SUBVOLUME_PATH")
+
+LOG_PATH="/var/log/btrfs"
+LOG_FILE="snapshot_$SUBVOLUME_NAME_$(date +%Y-%m-%d_%H-%M-%S).log"
+
+# Ensure log directory exists
+if [[ ! -d "$LOG_PATH" ]]; then
+    echo "Error: Log directory does not exist: $LOG_PATH"
+    exit 1
+fi
+
+# Check if the subvolume path exists and is a Btrfs subvolume
+if ! btrfs subvolume show "$SUBVOLUME_PATH" &>/dev/null; then
+    echo "Error: $SUBVOLUME_PATH is not a valid Btrfs subvolume."
+    exit 1
+fi
+
+# Define the snapshot directory
+SNAPSHOT_NAME="${SUBVOLUME_NAME}_snapshot_$(date +"%Y-%m-%d_%H-%M-%S")"
+SNAPSHOT_PATH="$SNAPSHOT_PARENT/$SNAPSHOT_NAME"
+
+
+# Create a read-only snapshot and log output
+{
+    echo "[$(date)] Starting Btrfs snapshot creation..."
+    btrfs subvolume snapshot -r "$SUBVOLUME_PATH" "$SNAPSHOT_PATH"
+
+    if [ $? -eq 0 ]; then
+        echo "[$(date)] Snapshot created at: $SNAPSHOT_PATH"
+    else
+        echo "[$(date)] Failed to create snapshot." | tee -a "$LOG_PATH/$LOG_FILE"
+        exit 1
+    fi
+} | tee -a "$LOG_PATH/$LOG_FILE"
+
+exit 0
--- ansible/templates/rsync_pull_job.sh
+++ ansible/templates/rsync_pull_job.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
-## Run rsync job, retry on errors until a maximum number of retries is reached
 
+
+# Check if four necessary arguments are provided
+if [[ "$#" -ne 4 ]]; then
+    echo "Usage: $(basename "$0") <rsync_user> <rsync_host> <source_path> <destination_path>"
+    exit 1
+fi
+
+RSYNC_USER=$1
+RSYNC_HOST=$2
+SOURCE_PATH=$3
+DESTINATION_PATH=$4
+
+LOG_PATH="/var/log/rsync"
+LOG_FILE="rsync_pull_job_$(date +\%Y-\%m-\%d_\%H-\%M-\%S).log"
+
+## Run rsync job, retry on errors until a maximum number of retries is reached
 MAX_RETRIES=10
-RSYNC_PULL_COMMAND=(rsync -av {{ rsync_user }}@{{ rsync_host }}:{{ rsync_source_path }} {{ rsync_destination_path }} -A -X --inplace --delete --log-file="/var/log/rsync/rsync_pull_job_$(date +\%Y-\%m-\%d_\%H-\%M-\%S).log" )
+#RSYNC_PULL_COMMAND=(rsync -av {{ rsync_user }}@{{ rsync_host }}:{{ rsync_source_path }} {{ rsync_destination_path }} -A -X --inplace --delete --log-file="/var/log/rsync/rsync_pull_job_$(date +\%Y-\%m-\%d_\%H-\%M-\%S).log" )
+RSYNC_PULL_COMMAND=(rsync -av $RSYNC_USER@$RSYNC_HOST:$SOURCE_PATH $DESTINATION_PATH -A -X --inplace --delete --log-file="$LOG_PATH/$LOG_FILE" )
 i=0
 
 while [ $i -lt $MAX_RETRIES ]; do
